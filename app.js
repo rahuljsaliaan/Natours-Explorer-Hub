79581adaf7d43b4f107bfae014e09cf01260f5b1
rahuljsaliaan <rahuljsaliaan@gmail.com>
2023-11-09 12:31:57 +0530

    added a new route to post request to add new tour using the post method

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 
+// middleware
+app.use(express.json());
+
 const port = 3000;
 
 const tours = JSON.parse(
@@ -17,6 +20,28 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  // Merging two objects
+  const tour = Object.assign({ id: newId }, req.body);
+
+  tours.push(tour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (error) => {
+      // 201 means created
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour,
+        },
+      });
+    }
+  );
 });
 
 app.listen(port, () => {
