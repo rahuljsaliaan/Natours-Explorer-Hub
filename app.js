90540ff3105f4dbece5a88878fcd42a17b085275
rahuljsaliaan <rahuljsaliaan@gmail.com>
2023-11-11 11:44:12 +0530

    Added Patch and Delete method routes for updating and deleting specific tour data

--- app.js
+++ app.js
@@ -22,6 +22,24 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const tour = tours.find((ele) => ele.id === id);
+
+  if (!tour)
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
 app.post('/api/v1/tours', (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   // Merging two objects
@@ -42,6 +60,42 @@ app.post('/api/v1/tours', (req, res) => {
       });
     }
   );
+});
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const tour = tours.find((ele) => ele.id === id);
+
+  if (!tour)
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: `<Updated tour ${Object.keys(req.body)} ${Object.values(
+        req.body
+      )} here...>`,
+    },
+  });
+});
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const tour = tours.find((ele) => ele.id === id);
+
+  if (!tour)
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
 });
 
 app.listen(port, () => {
