319cc4da0b0856cc417d010c6bd052910d283f0c
rahuljsaliaan <rahuljsaliaan@gmail.com>
2023-11-13 11:21:31 +0530

    Added a js function to import and delete all the data into and from mongodb using CLI with options --import and --delete (eg:  node dev-data/data/import-dev-data.js --import)

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,57 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+const Tour = require('../../models/tourModel');
+
+// Setting environment variables
+dotenv.config({ path: `${__dirname}../../../config.env` });
+
+// Reading the tours data from the file
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'),
+);
+
+// Connecting to Database
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connected successfully'));
+
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data successfully loaded');
+    // Aggressive way to stop and application
+    process.exit();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data successfully deleted');
+    process.exit();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Running the process through command line
+
+// node dev-data/data/import-dev-data.js --import
+if (process.argv[2] === '--import') importData();
+
+// node dev-data/data/import-dev-data.js --delete
+if (process.argv[2] === '--delete') deleteData();
