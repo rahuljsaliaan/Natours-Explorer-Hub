10beebe505cb74ce1eedb78c5b0a0a0c812de4a9
rahuljsaliaan <rahuljsaliaan@gmail.com>
2023-11-18 22:18:04 +0530

    Added functions to handle operational and programming errors on development and production environment

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,36 @@
+const sendErrorDev = (error, res) => {
+  res.status(error.statusCode).json({
+    status: error.status,
+    error: error,
+    message: error.message,
+    stack: error.stack,
+  });
+};
+
+const sendErrorProd = (error, res) => {
+  // operational error: send message to client
+  if (error.isOperational)
+    res.status(error.statusCode).json({
+      status: error.status,
+      message: error.message,
+    });
+  // Programming or other unknown error: error details are not leaked to the user
+  else {
+    // Log the error
+    console.error('ERROR 💥 ', error);
+
+    // Send generic message
+    res.status(500).json({
+      status: 500,
+      message: 'Something went very wrong',
+    });
+  }
+};
+
 module.exports = (error, req, res, next) => {
   error.statusCode = error.statusCode || 500;
   error.status = error.status || 'error';
 
-  res.status(error.statusCode).json({
-    status: error.status,
-    message: error.message,
-  });
+  if (process.env.NODE_ENV === 'development') sendErrorDev(error, res);
+  else if (process.env.NODE_ENV === 'production') sendErrorProd(error, res);
 };
