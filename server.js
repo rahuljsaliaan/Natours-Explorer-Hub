ebed26f3ab08408bd09bcca62c4e7ba508056e40
rahuljsaliaan <rahuljsaliaan@gmail.com>
2023-11-19 20:00:52 +0530

    Added a global error handler for synchronous code (uncaught exceptions) to shutdown the node application

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+// Handling synchronous exceptions
+process.on('uncaughtException', (error) => {
+  console.log('UNCAUGHT EXCEPTION 💥 Shutting down...');
+  console.error(error.name, error.message);
+  // NOTE: Stopping the node application in unhandled rejection is optional but here it is mandatory as in uncaught exception the entire node process will be in unclean state that is why we need to stop the application
+  process.exit(1);
+});
+
 // Setting environment variables
 dotenv.config({ path: `${__dirname}/config.env` });
 
@@ -28,11 +36,11 @@ const server = app.listen(port, () => {
   console.log(`App started on port ${port}`);
 });
 
-// handling rejections
+// Handling rejections
 // NOTE: This kind of error might occur which are out of reach such as Database password error etc.
 process.on('unhandledRejection', (error) => {
-  console.error(error.name, error.message);
   console.log('UNHANDLED REJECTION 💥 Shutting down...');
+  console.error(error.name, error.message);
 
   // NOTE: The server.close() method can be used to close the server gracefully, which means that all outstanding requests will be finished before the server is closed. To close the server gracefully, you can use the following code:
   server.close(() => {
