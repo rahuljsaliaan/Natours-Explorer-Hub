f20498e19078c6aa71f15aa50757fa7043437b48
rahuljsaliaan <rahuljsaliaan@gmail.com>
2023-11-19 21:32:51 +0530

    Added bcryptjs to hash passwords for security using a pre-save hook in the user model

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -23,6 +24,7 @@ const userSchema = new mongoose.Schema({
     minLength: 8,
   },
 
+  // This field is not persisted to the database (required only for validation purposes)
   passwordConfirm: {
     type: String,
     required: [true, 'Please confirm your password'],
@@ -36,8 +38,17 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-userSchema.pre('save', function (next) {
+userSchema.pre('save', async function (next) {
+  // Only run this function if password was actually modified
   if (!this.isModified('password')) return next();
+
+  // Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // Delete the passwordConfirm field
+  this.passwordConfirm = undefined;
+
+  next();
 });
 
 const UserModel = mongoose.model('User', userSchema);
