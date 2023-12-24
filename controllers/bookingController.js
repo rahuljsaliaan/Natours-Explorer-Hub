8bd733e66e75a4a54ec8fa0ee4ed269c2f0457c1
rahuljsaliaan <rahuljsaliaan@gmail.com>
2023-12-24 21:27:20 +0530

    Added: booking model with temporary solution to create booking

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,4 +1,5 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const Booking = require('../models/bookingModel');
 const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
 
@@ -33,7 +34,10 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
       },
     ],
     mode: 'payment',
-    success_url: `${req.protocol}://${req.get('host')}/`,
+    // NOTE: Not a secure way to do this. Use a webhook instead
+    success_url: `${req.protocol}://${req.get('host')}/?tour=${
+      req.params.tourId
+    }&user=${req.user.id}&price=${tour.price}`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
@@ -42,4 +46,17 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   res.status(200).json({ status: 'success', session });
   // 3) Send it to client
   res.status(200).json({ status: 'success', session });
+});
+
+exports.createBookingCheckout = catchAsync(async (req, res, next) => {
+  // FIXME: This is only temporary, because it's unsecure: everyone can make bookings without paying
+  const { tour, user, price } = req.query;
+
+  if (!tour && !user && !price) return next();
+
+  await Booking.create({ tour, user, price });
+
+  // NOTE: The reason why we are redirecting to the same page using the originalUrl is because we don't want to show the query string in the URL
+  // NOTE: originalUrl is the URL before the query string (this is used because the request might be from the template or from the API)
+  res.redirect(req.originalUrl.split('?')[0]);
 });
